40475480dac4dceebc53b27159530fd0ee110b6a
Yoko Ohtsu <148010448+Yoko272727@users.noreply.github.com>
2026-03-06 15:21:40 +0200

    Delete 01_basic/hba1c_timeseries.sql

--- 01_basic/hba1c_timeseries.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    PatientID,
-    VisitDate,
-    HbA1c,
-    HbA1c - LAG(HbA1c) OVER (PARTITION BY PatientID ORDER BY VisitDate) AS delta_hba1c
-FROM diabetes_records
-ORDER BY PatientID, VisitDate;
